aec01ee6392ecb0b10a825962f57cf95f8463a69
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2022-05-19 15:36:51 +0200

    Move QA tasks to DPG

--- DPG/Tasks/qaPIDTOF.h
+++ DPG/Tasks/qaPIDTOF.h
@@ -399,3 +399,83 @@ struct tofPidQa {
   makeProcessFunction(aod::pidTOFFullAl, Alpha);
 #undef makeProcessFunction
 };
+
+/// Task to produce the TOF QA plots for Beta
+struct tofPidBetaQa {
+  static constexpr int Np = 9;
+  static constexpr const char* pT[Np] = {"e", "#mu", "#pi", "K", "p", "d", "t", "^{3}He", "#alpha"};
+  static constexpr std::string_view hexpected[Np] = {"expected/El", "expected/Mu", "expected/Pi",
+                                                     "expected/Ka", "expected/Pr", "expected/De",
+                                                     "expected/Tr", "expected/He", "expected/Al"};
+  static constexpr std::string_view hexpected_diff[Np] = {"expected_diff/El", "expected_diff/Mu", "expected_diff/Pi",
+                                                          "expected_diff/Ka", "expected_diff/Pr", "expected_diff/De",
+                                                          "expected_diff/Tr", "expected_diff/He", "expected_diff/Al"};
+  static constexpr std::string_view hnsigma[Np] = {"nsigma/El", "nsigma/Mu", "nsigma/Pi",
+                                                   "nsigma/Ka", "nsigma/Pr", "nsigma/De",
+                                                   "nsigma/Tr", "nsigma/He", "nsigma/Al"};
+  HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+
+  Configurable<int> logAxis{"logAxis", 0, "Flag to use a log momentum axis"};
+  Configurable<int> nBinsP{"nBinsP", 400, "Number of bins for the momentum"};
+  Configurable<float> minP{"minP", 0.1f, "Minimum momentum in range"};
+  Configurable<float> maxP{"maxP", 5.f, "Maximum momentum in range"};
+  Configurable<int> nBinsBeta{"nBinsBeta", 4000, "Number of bins for the beta"};
+  Configurable<float> minBeta{"minBeta", 0, "Minimum beta in range"};
+  Configurable<float> maxBeta{"maxBeta", 2.f, "Maximum beta in range"};
+
+  void init(o2::framework::InitContext&)
+  {
+    const AxisSpec vtxZAxis{100, -20, 20, "Vtx_{z} (cm)"};
+    const AxisSpec tofAxis{10000, 0, 2e6, "TOF Signal"};
+    const AxisSpec betaAxis{nBinsBeta, minBeta, maxBeta, "TOF #beta"};
+    const AxisSpec etaAxis{100, -2, 2, "#it{#eta}"};
+    const AxisSpec colTimeAxis{100, -2000, 2000, "Collision time (ps)"};
+    const AxisSpec lAxis{100, 0, 500, "Track length (cm)"};
+    const AxisSpec ptResoAxis{100, 0, 0.1, "#sigma_{#it{p}_{T}}"};
+    AxisSpec ptAxis{nBinsP, minP, maxP, "#it{p}_{T} (GeV/#it{c})"};
+    AxisSpec pAxis{nBinsP, minP, maxP, "#it{p} (GeV/#it{c})"};
+    if (logAxis) {
+      ptAxis.makeLogaritmic();
+      pAxis.makeLogaritmic();
+    }
+
+    // Event properties
+    histos.add("event/tofsignal", "", HistType::kTH2F, {pAxis, tofAxis});
+    histos.add("event/tofbeta", "", HistType::kTH2F, {pAxis, betaAxis});
+    histos.add("event/tofbetaEvTimeTOF", "", HistType::kTH2F, {pAxis, betaAxis});
+    histos.add("event/eta", "", HistType::kTH1F, {etaAxis});
+    histos.add("event/length", "", HistType::kTH1F, {lAxis});
+    histos.add("event/pt", "", HistType::kTH1F, {ptAxis});
+    histos.add("event/p", "", HistType::kTH1F, {pAxis});
+  }
+
+  template <uint8_t i, typename T>
+  void fillParticleHistos(const T& t, const float tof, const float exp_diff, const float nsigma)
+  {
+    histos.fill(HIST(hexpected[i]), t.p(), tof - exp_diff);
+    histos.fill(HIST(hexpected_diff[i]), t.p(), exp_diff);
+    histos.fill(HIST(hnsigma[i]), t.p(), nsigma);
+  }
+  void process(soa::Join<aod::Tracks, aod::TracksExtra, aod::pidTOFbeta, aod::TrackSelection, aod::TOFSignal, aod::pidEvTimeFlags> const& tracks,
+               aod::Collisions const&)
+  {
+    for (auto const& track : tracks) {
+
+      if (!track.hasTOF()) { // Skipping tracks without TOF
+        continue;
+      }
+      if (!track.isGlobalTrack()) {
+        continue;
+      }
+      if (track.isEvTimeTOF()) {
+        histos.fill(HIST("event/tofbetaEvTimeTOF"), track.p(), track.beta());
+      }
+      histos.fill(HIST("event/tofbeta"), track.p(), track.beta());
+      histos.fill(HIST("event/length"), track.length());
+      histos.fill(HIST("event/eta"), track.eta());
+      histos.fill(HIST("event/tofsignal"), track.p(), track.tofSignal());
+      histos.fill(HIST("event/pt"), track.pt());
+      histos.fill(HIST("event/p"), track.p());
+    }
+  }
+};
